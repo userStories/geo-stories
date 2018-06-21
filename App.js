4a389fc537241ba034d4225da32830cf18e4e4d1
Minh Nguyen <minh@ellesa.com>
2018-06-21 15:46:47 -0500

    added map to app.js

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import { createStackNavigator } from 'react-navigation';
 import Home from './src/components/Home'
+import { View } from "react-native";
+import { MapView } from "expo";
 import store from './src/store/index'
+import MyMap from './components/MyMap'
 
 class App extends React.Component {
   render() {
     return (
       // <Provider store={store}>
+      <View>
         <StackNav />
+        <MyMap />
+        </View>
       // </Provider>
     );
   }
