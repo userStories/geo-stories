a516fe0d13bac8bc4ede6860d459984a9736262e
Chris <tophersjones@gmail.com>
2018-06-25 09:09:40 -0500

    commit

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,102 +1,78 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { StyleSheet, Text, MapView, View, Button, Image } from 'react-native';
-import { Video } from 'expo'
-import { getSinglePostThunk } from '../store'
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { StyleSheet, Text, MapView, View, Button, Image } from "react-native";
+import { Video } from "expo";
+import { getSinglePostThunk } from "../store";
 
 class SinglePost extends Component {
-
-<<<<<<< HEAD
   componentDidMount() {
-    const id = this.props.navigation.getParam('id', 'no input')
-    console.log('id in componentDidMount: ', id)
-    this.props.singlePostMaker(id)
+    const id = this.props.navigation.getParam("id", "no input");
+    console.log("id in componentDidMount: ", id);
+    this.props.singlePostMaker(id);
   }
-  
   render() {
-    console.log('this.props.singlePost.mediaLink.length: ', this.props.singlePost.mediaLink)
-    return (
+    const imageExt = ["jpeg", "jpg", "png", "gif"];
+    const videoExt = ["mp4", "mp3", "avi", "flv", "mov", "wmv"];
+    console.log(
+      "this.props.singlePost.mediaLink.length: ",
+      this.props.singlePost.mediaLink
+    );
+    return this.props.singlePost.mediaLink ? (
       <View style={styles.OuterViewWrap}>
-        <Text>Title</Text>
-        {this.props.singlePost.mediaLink ?
-          <Video
-            source={{ uri: this.props.singlePost.mediaLink }}
-            rate={1.0}
-            volume={1.0}
-            muted={false}
-            resizeMode="cover"
-            shouldPlay
-            isLooping
-            style={styles.imageWrap}
-          /> : null}
+        <Text>{this.props.singlePost.title}</Text>
+        {videoExt.indexOf(this.props.singlePost.mediaLink.slice(-3)) !== -1 ||
+        videoExt.indexOf(this.props.singlePost.mediaLink.slice(-4)) !== -1 ? (
+            <Video
+              source={{ uri: this.props.singlePost.mediaLink }}
+              rate={1.0}
+              volume={1.0}
+              muted={false}
+              resizeMode="cover"
+              shouldPlay
+              isLooping
+              style={styles.imageWrap}
+            />
+          ) : (
+            <Image
+              style={styles.imageWrap}
+              source={{ uri: this.props.singlePost.mediaLink }}
+            />
+          )}
         <Text>{this.props.singlePost.text}</Text>
       </View>
-
-    )
+    ) : (
+      <View style={styles.OuterViewWrap}>
+        <Text>{this.props.singlePost.title}</Text>
+        <Text>{this.props.singlePost.text}</Text>
+      </View>
+    );
   }
-=======
-    componentDidMount(){
-        const id = this.props.navigation.getParam('id', 'no input')
-        console.log('id in componentDidMount: ', id)
-        this.props.singlePostMaker(id)
-    }
-    render(){
-        const imageExt = ['jpeg', 'jpg', 'png', 'gif']
-        const videoExt = ['mp4', 'mp3', 'avi', 'flv', 'mov', 'wmv'];
-        console.log('this.props.singlePost.mediaLink.length: ', this.props.singlePost.mediaLink)
-        return (
-            this.props.singlePost.mediaLink ?
-            <View style={styles.OuterViewWrap}>
-                <Text>{this.props.singlePost.title}</Text>
-                {videoExt.indexOf(this.props.singlePost.mediaLink.slice(-3)) !== -1 || videoExt.indexOf(this.props.singlePost.mediaLink.slice(-4)) !== -1 ? 
-                <Video 
-                source={{uri: this.props.singlePost.mediaLink}} 
-                rate={1.0}
-                volume={1.0}
-                muted={false}
-                resizeMode="cover"
-                shouldPlay
-                isLooping
-                style={styles.imageWrap} 
-                />: 
-                <Image style={styles.imageWrap}source={{uri: this.props.singlePost.mediaLink}} /> 
-                }
-                <Text>{this.props.singlePost.text}</Text>
-            </View>:
-            <View style={styles.OuterViewWrap}>
-                <Text>{this.props.singlePost.title}</Text>
-                <Text>{this.props.singlePost.text}</Text>
-            </View>
-
-        )
-    }
->>>>>>> 3b87fb941d645e73ef136066d9a01b141dd1b067
 }
-
 
 const styles = StyleSheet.create({
   OuterViewWrap: {
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   imageWrap: {
     width: 350,
-    height: 300,
+    height: 300
   }
-})
+});
 const mapStateToProps = state => {
-  console.log('state in mapState: ', state)
+  console.log("state in mapState: ", state);
   return {
     singlePost: state.postReducer.singlePost
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-    singlePostMaker: (postId) => dispatch(getSinglePostThunk(postId))
-  }
-}
+    singlePostMaker: postId => dispatch(getSinglePostThunk(postId))
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SinglePost)
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SinglePost);
--- src/store/postReducer.js
+++ src/store/postReducer.js
@@ -5,60 +5,60 @@ const GET_SINGLE_POST = "GET_SINGLE_POST"
 const GET_ALL_POSTS = "GET_ALL_POSTS"
 
 const getSinglePost = post => {
-    return {
-        type: GET_SINGLE_POST,
-        post
-    }
+  return {
+    type: GET_SINGLE_POST,
+    post
+  }
 }
 
 const getAllPosts = posts => {
-    return {
-        type:GET_ALL_POSTS,
-        posts 
-    }
+  return {
+    type: GET_ALL_POSTS,
+    posts
+  }
 }
 
 
 const initialState = {
-    singlePost: {},
-    allPosts: []
+  singlePost: {},
+  allPosts: []
 }
 
-export const getSinglePostThunk = postId =>{
-    return async dispatch => {
-        try{
-            console.log('postId in thunk: ', postId)
-            const {data} = await axios.get(`http://localhost:8080/api/posts/${postId}`)
-            // const {data} = await axios.get(`http://172.17.20.159:8080/api/posts/${postId}`)
-            console.log('data in thunk: ', data)
-            dispatch(getSinglePost(data))
-        } catch(err){
-            console.error(err)
-        }
+export const getSinglePostThunk = postId => {
+  return async dispatch => {
+    try {
+      console.log('postId in thunk: ', postId)
+      const { data } = await axios.get(`http://localhost:8080/api/posts/${postId}`)
+      // const {data} = await axios.get(`http://172.17.20.159:8080/api/posts/${postId}`)
+      console.log('data in thunk: ', data)
+      dispatch(getSinglePost(data))
+    } catch (err) {
+      console.error(err)
     }
+  }
 }
 
 export const getAllPostsThunk = () => {
-    return async (dispatch) =>{
-        try{
-            const {data} = await axios.get('http://localhost:8080/api/posts')
-            // const {data} = await axios.get(`http://172.17.20.159:8080/api/posts`)
-            console.log('data in all posts thunk: ', data)
-            dispatch(getAllPosts(data))
-        } catch(err){
-            console.error(err);
-        }
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get('http://localhost:8080/api/posts')
+      // const {data} = await axios.get(`http://172.17.20.159:8080/api/posts`)
+      console.log('data in all posts thunk: ', data)
+      dispatch(getAllPosts(data))
+    } catch (err) {
+      console.error(err);
     }
-    
+  }
+
 }
 
-export const postReducer = (state = initialState, action) =>{
-    switch(action.type){
-        case GET_SINGLE_POST:
-            return {...state, singlePost: action.post}
-        case GET_ALL_POSTS:
-            return {...state, allPosts: action.posts}
-        default:
-            return state
-    }
+export const postReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SINGLE_POST:
+      return { ...state, singlePost: action.post }
+    case GET_ALL_POSTS:
+      return { ...state, allPosts: action.posts }
+    default:
+      return state
+  }
 }
