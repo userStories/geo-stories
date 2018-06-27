4c4c346b399efe936d751e68025d164a00283376
Shaheed Shamsi <shamsishaheed@gmail.com>
2018-06-27 16:54:27 -0500

    video storage to cloud working

--- src/store/addPostReducer.js
+++ src/store/addPostReducer.js
@@ -3,9 +3,6 @@ import { ImageManipulator } from 'expo'
 // import 'whatwg-fetch'
 // import RNFetchBlob from 'rn-fetch-blob'
 
-
-
-
 const ADD_NEW_POST = 'ADD_NEW_POST'
 
 const addNewPost = (newPost) => {
@@ -18,22 +15,15 @@ const addNewPost = (newPost) => {
 
 export const addNewPostThunk = (info) => {
   return async (dispatch) => {
-
-    // "change method to post"
-    // "change header to form data???"
-    // "deploy api to heroku and asssssdjust url"
     try {
-
-      // MEDIA POST PART
       const formData = new FormData()
       const uriParts = info.uri.split('.')
       let fileType = uriParts[uriParts.length - 1]
+      const fetcher = null
 
-      console.log('info.uri', info.uri)
-      console.log('filetype', fileType)
       if (fileType === 'jpg') {
         fileType = 'image/jpg'
-        console.log('entered here')
+
       } else if (fileType === 'mov') {
         fileType = 'video/quicktime'
       }
@@ -52,11 +42,8 @@ export const addNewPostThunk = (info) => {
             'Content-Type': 'multipart/form-data',
           },
         }
-
         const url = 'http://172.17.20.5:8080/api/posts/media'
-        console.log('url', url)
-        console.log('options', options)
-        await fetch(url, options)
+        fetcher = await fetch(url, options)
       } else {
         formData.append('mediaPost', {
           uri: info.uri,
@@ -71,40 +58,11 @@ export const addNewPostThunk = (info) => {
             'Content-Type': 'multipart/form-data',
           },
         }
-
         const url = 'http://172.17.20.5:8080/api/posts/media'
-        console.log('url', url)
-        console.log('options', options)
-        await fetch(url, options)
-        
+        fetcher = await fetch(url, options)
       }
-
-      // if (fileType = 'video/quicktime') {
-
-      //   const myBlob = new Blob([info.uri], { type: fileType })
-
-      //   formData.append('mediaPost', {
-      //     file: Blob,
-      //     name: `${info.uri}`,
-      //     type: fileType,
-      //   })
-      //   let options = {
-      //     method: 'POST',
-      //     body: formData,
-      //     headers: {
-      //       Accept: 'application/json',
-      //       'Content-Type': 'multipart/form-data',
-      //     }
-      //   }
-      //   const url = 'http://172.17.20.5:8080/api/posts/media'
-      //   const mediaPost = await fetch(url, options)
-      // }
-
-
-
-        
-      
-
+      const response = await fetcher.json()
+      let mediaUrl = response.mediaUrl
 
     } catch (err) {
       console.error('error in thunk', err.message)
