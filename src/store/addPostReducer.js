97dab28478cbbfc15883405a86d7a90cd54a341c
Shaheed Shamsi <shamsishaheed@gmail.com>
2018-06-28 09:29:00 -0500

    conflicts resolved

--- src/store/addPostReducer.js
+++ src/store/addPostReducer.js
@@ -13,39 +13,14 @@ const addNewPost = (newPost) => {
   }
 }
 
-
+// remove this comment
 export const addNewPostThunk = (info) => {
   return async (dispatch) => {
     try {
-<<<<<<< HEAD
       const formData = new FormData()
       const uriParts = info.uri.split('.')
       let fileType = uriParts[uriParts.length - 1]
       const fetcher = null
-=======
-      
-      let newObj = await Expo.ImageManipulator.manipulate(info.uri, null, { base64: true })
-      // let formData = new FormData()
-      console.log('newobj', newObj)
-      console.log('info object', info)
-      // console.log('specific uri', info.uri)
-      // formData.append('file', newObj)
-
-
-
-      // const rawResponse = await fetch('http://172.17.20.159:8080/api/posts/', {
-      const rawResponse = await fetch(`http://${HOST_IP_ADDRESS}:8080/api/posts/`, {
-        method: 'POST',
-        headers: {
-          'X-AYLIEN-TextAPI-Application-ID': '6aca562c',
-          'X-AYLIEN-TextAPI-Application-Key': '4cc1a266da1f4cc14396b900072e9a1d',
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-        body: qs.stringify(newObj)
-      })
-
-
->>>>>>> 0d80007d1f5bca2573c5791edc410e5c8674904d
 
       if (fileType === 'jpg') {
         fileType = 'image/jpg'
